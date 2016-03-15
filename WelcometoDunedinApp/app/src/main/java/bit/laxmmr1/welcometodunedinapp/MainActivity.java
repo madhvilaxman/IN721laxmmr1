dc5c2d64594959ca0434adf3499885baa24b934b
Madhvi Laxman (1000029639) <LAXMMR1@student.op.ac.nz>
2016-03-15 15:12:40 +1300

    Welcome to Dunedin - Switchstatement

--- WelcometoDunedinApp/app/src/main/java/bit/laxmmr1/welcometodunedinapp/MainActivity.java
+++ WelcometoDunedinApp/app/src/main/java/bit/laxmmr1/welcometodunedinapp/MainActivity.java
@@ -1,5 +1,6 @@
 package bit.laxmmr1.welcometodunedinapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,33 +11,58 @@ import android.widget.Toast;
 
 import java.lang.reflect.Array;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         setUpActivitiesGroupList();
 
-        final ListView changeActivity =(ListView) findViewById(R.id.listView);
+        ListView changeActivity = (ListView) findViewById(R.id.listView);
+        changeActivity.setOnItemClickListener(new ListViewClickHandler());
+    }//end Method OnCreate
 
-
-
-    public void setUpActivitiesGroupList() {
+    public void setUpActivitiesGroupList()
+    {
             String[] groups = {"Services", "Fun Things To Do", "Dining", "Shopping"};
             //An ArrayAdapter
-            ArrayAdapter<String> activitesAdapter = new ArrayAdapter<String>(this, R.layout.activity_main, groups);
+            ArrayAdapter<String> activitesAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_2, groups);
             //ListView binded with the Adapter
             ListView activitiesGroupListView = (ListView) findViewById(R.id.listView);
             activitiesGroupListView.setAdapter(activitesAdapter);
-    }
+    }//End Method SetupActivities
 
-    public class ListViewWithToastHandler implements AdapterView.OnItemClickListener
+    public class ListViewClickHandler implements AdapterView.OnItemClickListener
     {
         @Override
-        public void onItemClick (AdapterView<?> parent, View view, int position, long id){
-            String clickedItemString = (String)parent.getItemAtPosition(position).toString();
-            Toast.makeText(MainNavigation.this, clickedItemString, Toast.LENGTH_LONG.show);
-        }
-    }
-}
-}
+        public void onItemClick (AdapterView<?> parent, View view, int position, long id)
+        {
+            String clickedItemString = parent.getItemAtPosition(position).toString();
+            Intent goToIntent;
+
+            switch (clickedItemString)
+            {
+                case "Services";
+                    goToIntent = new Intent(MainActivity.this, ServicesActivity.class);
+                    break;
+                case "Fun Things To Do";
+                    goToIntent = new Intent(MainActivity.this, FunThingsTodo.class);
+                    break;
+                case "Dining";
+                    goToIntent = new Intent(MainActivity.this, DiningActivity.class);
+                    break;
+                case "Shopping";
+                    goToIntent = new Intent(MainActivity.this, Shopping.class);
+                    break;
+                  default:
+                      goToIntent = null;
+            }//end Switch
+
+            if (goToIntent != null)
+                startActivity(goToIntent);
+
+        }//End Method onItemClick
+    }//End Class ListView with Handler
+}//End Main Activity
